7d2eb4fbf25e085352525d349e403dba29f0dc06
leebokeum <leebokeum87@gmail.com>
2018-08-27 17:33:10 +0900

    image util

--- src/main/java/com/leebokeum/common/FileUtil.java
+++ src/main/java/com/leebokeum/common/FileUtil.java
@@ -11,11 +11,11 @@ import com.leebokeum.dao.vo.Content;
 import org.springframework.web.multipart.MultipartFile;
 
 public class FileUtil {
-	private final String imgOriginalPath = "C:\\dev\\origin-file\\";
-	private final String targetFilePath = "C:\\dev\\file\\";
+	//private final String imgOriginalPath = "C:\\dev\\origin-file\\";
+	//private final String targetFilePath = "C:\\dev\\target-file\\";
 
-	//private final String imgOriginalPath = "/home/ec2-user/origin-upload/";
-	//private final String targetFilePath = "/home/ec2-user/upload/";
+	private final String imgOriginalPath = "/home/ec2-user/origin-upload/";
+	private final String targetFilePath = "/home/ec2-user/upload/";
 
 	
 	public void parseInsertFileHeader(Board content, MultipartFile multipartFile) throws Exception{
@@ -23,19 +23,26 @@ public class FileUtil {
 		String originalFileExtension = null; 
 		String storedFileName = null; 
 
-		File file = new File(imgOriginalPath);
-		if(file.exists() == false){
-			file.mkdirs();
+		//이미지를 저장할 경로가 없다면 생성한다.
+		File originFile = new File(imgOriginalPath);
+		if(originFile.exists() == false){
+			originFile.mkdirs();
+		}
+
+		//이미지를 저장할 경로가 없다면 생성한다.
+		File targetFile = new File(targetFilePath);
+		if(targetFile.exists() == false){
+			targetFile.mkdirs();
 		}
 
 		originalFileName = multipartFile.getOriginalFilename();
 		originalFileExtension = originalFileName.substring(originalFileName.lastIndexOf(".") + 1);
 		storedFileName = CommonUtil.getRandomString() + "." + originalFileExtension;
-		file = new File(imgOriginalPath + storedFileName);
-		multipartFile.transferTo(file);
+		originFile = new File(imgOriginalPath + storedFileName);
+		multipartFile.transferTo(originFile);
 
 		ImgResizeRatio imgResizeRatio = new ImgResizeRatio();
-		imgResizeRatio.imgResize(imgOriginalPath + storedFileName, targetFilePath + storedFileName, originalFileExtension , 300, 300);
+		imgResizeRatio.imgResize(imgOriginalPath + storedFileName, targetFilePath + storedFileName, originalFileExtension , 800, 800);
 
 		content.setOriginImg1(originalFileName);
 		content.setImg1(storedFileName);
